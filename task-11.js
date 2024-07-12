16f65709d3f5f276a4e393585e514d021912a1e8
isratjmn <mnisrat.j@gmail.com>
2024-07-12 17:38:17 +0600

    task-8, 11 Finished

--- task-11.js
+++ task-11.js
@@ -1,3 +1,15 @@
 /* 11. Task: Functional Programming - Reduce
 
-Write A Function That Uses The Reduce Function To Calculate The Total Value Of An Array Of Objects With A 'Quantity' And 'Price' Property.Interview */
+Write A Function That Uses The Reduce Function To Calculate The Total Value Of An Array Of Objects With A 'Quantity' And 'Price' Property. */
+
+const items = [
+	{ quantity: 2, price: 10 },
+	{ quantity: 3, price: 15 },
+	{ quantity: 1, price: 5 },
+];
+const CalculateTotalValue = (items) => {
+	return items.reduce((acc, item) => acc + item.quantity * item.price, 0);
+};
+
+const totalValue = CalculateTotalValue(items);
+console.log(totalValue);
--- task-8.js
+++ task-8.js
@@ -1,7 +1,14 @@
 /* 8. Task: Unique Values
 
 Create An Array Of Numbers With Some Duplicate Values. Write A Function To Filter Out The Duplicate Values And Return A New Array With Only Unique Numbers. Print The Result.
+ */
 
-09. Task: Find Maximum Value:
+const numbers = [
+	1, 2, 3, 49, 4, 4, 5, 67, 67, 70, 75, 80, 48, 77, 36, 84, 84, 92, 92, 92,
+];
+const uniqueValues = (numbers) => {
+	return [...new Set(numbers)];
+};
 
-Write A Function That Takes An Array Of Numbers And Returns The Maximum Value. */
+const uniqueNumbers = uniqueValues(numbers);
+console.log(uniqueNumbers);
