4aabaaef370028d8de8c1d83e91b207204ab4cd6
isratjmn <mnisrat.j@gmail.com>
2024-07-12 17:14:20 +0600

    task-5 Finished

--- task-2.js
+++ task-2.js
@@ -1,3 +1,17 @@
 /* 2.Task: Object Manipulation
 
 Create An Array Of Objects Representing Books With Properties Like Title, Author, And Year. Write A Function That Takes The Array And Returns A New Array With Only The Book Titles. Print The Result. */
+
+const books = [
+	{ title: "To Kill a Mockingbird", author: "Harper Lee", year: 1960 },
+	{ title: "1984", author: "George Orwell", year: 1949 },
+	{ title: "Animal Farm", author: "Abelard Hughes", year: 1945 },
+	{ title: "The Great Gatsby", author: "F. Scott Fitzgerald", year: 1925 },
+];
+const getBookTitle = (books) => {
+	const bookTitles = books.map((book) => book.title);
+	return bookTitles;
+};
+
+const titles = getBookTitle(books);
+console.log(titles);
--- task-5.js
+++ task-5.js
@@ -1,3 +1,23 @@
 /* 5.Task: Find And Modify
 
 Write A Function That Searches An Array Of Objects For A Specific Person By Name. If Found, Modify Their Age Property. Print The Updated Array */
+
+const people = [
+	{ name: "John", age: 30 },
+	{ name: "Alice", age: 28 },
+	{ name: "Bob", age: 35 },
+	{ name: "Charlie", age: 32 },
+	{ name: "David", age: 37 },
+];
+
+const findModifyNewAge = (people, name, newAge) => {
+	const findPerson = people.find((person) => person.name === name);
+	if (findPerson) {
+		findPerson.age = newAge;
+		console.log(people);
+	} else {
+		console.log("Person not found.");
+	}
+};
+const updatedPeople = findModifyNewAge(people, "David", 10);
+console.log(updatedPeople);
